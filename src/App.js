2570e525b0d55eedc7ee130899b6e06d780b137a
Mjawad703 <m.jawad3300@gmail.com>
2021-02-26 16:54:45 +0500

    Created a header page deleted unnessary files

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import '../src/App.css'
 import HomePage from '../src/pages/homepage/homepage.component.jsx'
 import Shop from '../src/pages/shop/shop.component.jsx'
-import { Route } from 'react-router-dom'
+import Header from '../src/components/header/header.component.jsx'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={Shop} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={Shop} />
+      </Switch>
     </div>
   );
 }
