883aa7ccb1ee5cb1c5c022b771cc10e4bd33a6e8
Mjawad703 <m.jawad3300@gmail.com>
2021-03-01 15:18:10 +0500

    added otherProps to custom button component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import '../custom-button/custom-button.style.scss'
 
 const CustomButton = ({ children, ...otherProps }) => (
-    <button className="custom-button">
+    <button className="custom-button" {...otherProps}>
         {children}
     </button>
 )
